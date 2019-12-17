3a8799bdeb19bc2fdfb1d1d15a1bde6ac098bfa2
demon <xiangxiang12345@outlook.com>
2019-12-17 15:38:56 +0800

    demon

---
+++ src/main/java/com/example/demo/api/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String HelloController(){
+        return "hello word";
+    }
+}
